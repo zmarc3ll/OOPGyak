59185fd2f14b2ad1e970f99bed93b3273817b811
zmarc3ll <zubermarcellpetrik@gmail.com>
2022-09-27 21:22:21 +0200

    kész feladat

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
-let zenekLista=[];
+let zeneList = [];
 
-class Zenek{
+    class Song {
+        #dal;
+        #hossz;
 
-    constructor(cim, hossz){
-        this.cim = cim;
-        this.hossz = hossz;
+    constructor(dal, hossz) {
+        this.#dal = dal;
+        this.#hossz = hossz;
     }
 
+    get dal() {
+        return this.#dal; 
+    }
+
+    get hossz() {
+        return this.#hossz;
+    }
 }
-zenekLista.push(document.getElementById('zeneszam').innerHTML, document.getElementById('zeneHossz').value);
-let zenek=new Zenek(zenekLista.cim,zenekLista.hossz)
-function zeneszamHozzaad(){
-    document.getElementById('zeneszam').innerHTML=zenek.cim;
-}
-document.getElementById('hozzaad').addEventListener('click',zeneszamHozzaad());
+    function addToList() {
+        var z = new Song(document.getElementById("song").value, parseInt(document.getElementById("length").value));
+        zeneList.push(z);
+        var osszeadva = sum();
+        document.getElementById("sum").innerText ="Össz. hosszúság: " + osszeadva;
+    }
+
+    function sum() {
+        var sum = 0;
+        for (let i = 0; i < zeneList.length; i++) {
+            sum += zeneList[i].length;
+        }
+        return sum;
+    }
+
+   
+        function init() {
+        document.getElementById("add").addEventListener("click", addToList);
+    }
+
+        document.addEventListener("DOMContentLoaded", init);
+
+
